032022cbb24deb9f045bf3e1519698283df3d9b0
Oscar Thelander <oscarga.thelander@gmail.com>
2022-08-30 23:11:02 +0200

    Added config-inputs

---
+++ src/config.py
@@ -0,0 +1,10 @@
+# Path-variables
+INPUT_FOLDER = "../input/"
+TRAINING_DATA = INPUT_FOLDER + "train_data.ftr"
+TRAINING_LABELS = INPUT_FOLDER + "training_labels.csv"
+TEST_DATA = INPUT_FOLDER + "test_data.ftr"
+
+MODEL_OUTPUT = "../models/"
+
+# Constants
+SEED = 42
