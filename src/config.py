ef6e14a4d87b3c4df40e62ab9031ec1f3eef9bb2
Oscar Thelander <oscarga.thelander@gmail.com>
2022-08-31 21:13:44 +0200

    Updated config with new paths

--- src/config.py
+++ src/config.py
@@ -1,10 +1,26 @@
 # Path-variables
 INPUT_FOLDER = "../input/"
-TRAINING_DATA = INPUT_FOLDER + "train_data.ftr"
-TRAINING_LABELS = INPUT_FOLDER + "training_labels.csv"
+RAW_DATA = INPUT_FOLDER + "train_data.ftr"
+TRAINING_LABELS = INPUT_FOLDER + "train_labels.csv"
 TEST_DATA = INPUT_FOLDER + "test_data.ftr"
+
+TRANSFORMED_DATA = INPUT_FOLDER + "transformed_train.csv"
+TRAIN_DATA = INPUT_FOLDER + "train_folds.csv"
 
 MODEL_OUTPUT = "../models/"
 
 # Constants
 SEED = 42
+CAT_FEATURES = [
+    "B_30",
+    "B_38",
+    "D_114",
+    "D_116",
+    "D_117",
+    "D_120",
+    "D_126",
+    "D_63",
+    "D_64",
+    "D_66",
+    "D_68",
+]
