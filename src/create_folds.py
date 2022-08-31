3c99e80948dbb68a321a81a2a7b210eda0873888
Oscar Thelander <oscarga.thelander@gmail.com>
2022-08-31 21:14:01 +0200

    Added k-fold split to transformed data

--- src/create_folds.py
+++ src/create_folds.py
@@ -3,18 +3,16 @@ import config
 from sklearn import model_selection
 
 if __name__ == "__main__":
-    
-    # Need to aggregate data before K-fold?
     print("Reading data!")
-    
-    df = pd.read_feather(config.TRAINING_DATA)
-    
+    df = pd.read_csv(config.TRANSFORMED_DATA)
     df["kfold"] = -1
 
     print("Data loaded!")
     df = df.sample(frac=1).reset_index(drop=True)
 
-    kf = model_selection.StratifiedKFold(n_splits=5, shuffle=False, random_state=config.SEED)
+    kf = model_selection.StratifiedKFold(
+        n_splits=5, shuffle=False, random_state=config.SEED
+    )
 
     print("Starting loop!")
     for fold, (train_idx, val_idx) in enumerate(kf.split(X=df, y=df.target.values)):
@@ -22,4 +20,4 @@ if __name__ == "__main__":
         df.loc[val_idx, "kfold"] = fold
 
     print("Saving data!")
-    df.to_csv("../input/train_folds.csv", index=False)
+    df.to_csv(config.TRAIN_DATA, index=False)
