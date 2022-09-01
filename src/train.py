770e021aed818d070a9d7c35f92d855b1aba6147
Oscar Thelander <oscarga.thelander@gmail.com>
2022-09-01 20:40:38 +0200

    added imports

--- src/train.py
+++ src/train.py
@@ -1,10 +1,17 @@
+import argparse 
+import os
+import joblib
+
 import pandas as pd
-import numpy as np
+from sklearn import metrics
+
 import config
+import model_dispatcher
 
 
 def main():
     df = pd.read_csv(config.TRAIN_DATA)
+
 
 if __name__ == "__main__":
     main()
