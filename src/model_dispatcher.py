c1cded58cac18749ea7bfe65e557b1d1aecd534c
Oscar Thelander <oscarga.thelander@gmail.com>
2022-09-01 20:39:38 +0200

    Added more classifiers

--- src/model_dispatcher.py
+++ src/model_dispatcher.py
@@ -1,8 +1,19 @@
 # model_dispatcher.py
 
-from sklearn import tree
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
 
 models = {
-    "decision_tree_gini": tree.DecisionTreeClassifier(criterion="gini"),
-    "decision_tree_entropy": tree.DecisionTreeClassifier(criterion="entropy"),
+    "decision_tree_gini": DecisionTreeClassifier(criterion="gini"),
+    "decision_tree_entropy": DecisionTreeClassifier(criterion="entropy"),
+    "log_reg": LogisticRegression(),
+    "k_neighbors": KNeighborsClassifier(),
+    "svc": SVC(),
+    "random_forest": RandomForestClassifier(),
+    "adaboost": AdaBoostClassifier(),
+    "naive_bayes": GaussianNB(),
 }
